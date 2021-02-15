55a3ad31247176b2a02c6d81884b1e402920b9ce
Brycen Dorsay <dorsay@live.ca>
2021-02-14 18:35:14 -0800

    start of containerized node lambda

---
+++ src/auth/src/login.ts
@@ -0,0 +1,7 @@
+import 'source-map-support/register';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import createResponse from './create-response';
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  return createResponse(200, { message: 'Login' });
+};
