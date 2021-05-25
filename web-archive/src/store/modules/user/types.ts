f4eafe5229ca402a8a4b8dfa097c065220062108
Brycen Dorsay <dorsay@live.ca>
2021-05-24 19:09:32 -0700

    removed web archive

--- web-archive/src/store/modules/user/types.ts
+++
@@ -1,7 +0,0 @@
-export type LoginStatus = 'pending' | 'loggedIn' | 'loggedOut';
-
-export interface UserState {
-  sessionToken: string | null;
-  sessionExpiration: number | null;
-  loginStatus: LoginStatus;
-}
